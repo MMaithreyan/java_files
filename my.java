85540c6cb9cfc176d7adbb99d01b8aa1ff2e155b
MMaithreyan <maithreyanm8@gmail.com>
2022-03-14 23:56:36 +0530

    name changed

---
+++ my.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class my{
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+       String arr[]=new String[a];
+
+       for(int i=0;i<=a;i++){
+           arr[i]=sc.nextLine();
+       }
+
+       Arrays.sort(arr);
+       
+       for(int j=0;j<a;j++){
+           System.out.println(arr[j]);
+       }
+    }
+}
